bb0bef45dd929d41d147d8e4c90f0000ade5ed22
alexanderfilip1 <showgammer@gmail.com>
2024-08-16 13:05:36 +0300

    Change logo and burger menu UI

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,6 +3,7 @@ import "animate.css"; // Import Animate.css
 import "../assets/css/Header.css";
 import MobileHeader from "./MobileHeader";
 import links from "../components/HeaderLinks";
+import logo from "../assets/images/logo.svg";
 
 export default function Header() {
   const [mobileHeader, setMobileHeader] = useState(false);
@@ -13,7 +14,7 @@ export default function Header() {
       setAnimation("fadeOutUp");
       setTimeout(() => {
         setMobileHeader(false);
-      }, 900);
+      }, 750);
     } else {
       setMobileHeader(true);
       setAnimation("fadeInDown");
@@ -24,7 +25,7 @@ export default function Header() {
     <header className="header bgBeige container">
       <nav className="header__navbar">
         <a href="/" className="header__logo">
-          Nail Artist
+          <img src={logo} alt="Anastasya Nails Artist logo" />
         </a>
         <ul className="header__navbar--list">
           {links.map((item, index) => {
@@ -53,24 +54,43 @@ export default function Header() {
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
           >
-            <path
-              d="M4 18L20 18"
-              stroke="#000000"
-              strokeWidth="2"
-              strokeLinecap="round"
-            />
-            <path
-              d="M4 12L20 12"
-              stroke="#000000"
-              strokeWidth="2"
-              strokeLinecap="round"
-            />
-            <path
-              d="M4 6L20 6"
-              stroke="#000000"
-              strokeWidth="2"
-              strokeLinecap="round"
-            />
+            {mobileHeader ? (
+              <>
+                <path
+                  d="M6 18L18 6"
+                  stroke="#000000"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                />
+                <path
+                  d="M6 6L18 18"
+                  stroke="#000000"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                />
+              </>
+            ) : (
+              <>
+                <path
+                  d="M4 18L20 18"
+                  stroke="#000000"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                />
+                <path
+                  d="M4 12L20 12"
+                  stroke="#000000"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                />
+                <path
+                  d="M4 6L20 6"
+                  stroke="#000000"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                />
+              </>
+            )}
           </svg>
         </div>
         {mobileHeader && (
