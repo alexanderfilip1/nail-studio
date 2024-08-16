ef898af0cdba072e5c279f60d97a3ca642451e0b
alexanderfilip1 <showgammer@gmail.com>
2024-08-16 17:43:07 +0300

    Add blur effect to main content when mobile header is open

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,12 +1,13 @@
-import React from "react";
+import { useState } from "react";
 import Header from "../components/Header";
 import HeroSection from "../components/HeroSection";
 import AboutSection from "../components/AboutSection";
 export default function HomePage() {
+  const [mobileHeader, setMobileHeader] = useState(false);
   return (
     <>
-      <Header />
-      <main className="main">
+      <Header mobileHeader={mobileHeader} setMobileHeader={setMobileHeader} />
+      <main className={mobileHeader ? "main blur" : "main"}>
         <HeroSection />
         <AboutSection />
       </main>
