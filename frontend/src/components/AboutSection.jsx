2674e1cb1b10eafb48359e8c038719135e11872e
alexanderfilip1 <showgammer@gmail.com>
2024-08-16 19:16:48 +0300

    Improve Section UI

--- frontend/src/components/AboutSection.jsx
+++ frontend/src/components/AboutSection.jsx
@@ -1,34 +1,39 @@
 import React from "react";
 import "../assets/css/AboutSection.css";
+import nailsWork from "../assets/images/nails_work.webp";
 
 export default function AboutSection() {
   return (
-    <>
-      <section className="about__section bgBrown container">
-        <article className="about__section--content">
-          <h1>About Our</h1>
-          <p>
-            Meet the passionate team behind our nail artistry. From innovative
-            techniques to personalized designs.
-          </p>
-        </article>
-        <div className="about__section--cards">
-          <article className="about__section--card">
-            <h2>Our Story</h2>
-            <p>Crafting Unique Nail Designs for Every</p>
-          </article>
-
-          <article className="about__section--card">
-            <h2>Services</h2>
-            <p>Elevate Your Look with Our Wide Range of Nail</p>
-          </article>
-
-          <article className="about__section--card">
-            <h2>Contact Us</h2>
-            <p>Crafting Unique Nail Designs for Every</p>
-          </article>
+    <section className="about__section bgBrown container">
+      <article className="about__section--content">
+        <h1>About Our Team</h1>
+        <p>
+          Meet the passionate team behind our nail artistry. From innovative
+          techniques to personalized designs, we bring creativity and expertise
+          to every manicure.
+        </p>
+      </article>
+      <div className="about__image--text">
+        <div className="about__image fadeInLeftBig">
+          <img
+            src={nailsWork}
+            loading="lazy"
+            alt="Anastasia working on nail art"
+          />
         </div>
-      </section>
-    </>
+        <div className="about__side-text fadeInRightBig">
+          <p>
+            I am a skilled nail artist with over 3 years of experience
+            specializing in manicures and pedicures. My passion for nail
+            artistry is reflected in the meticulous care and creativity I bring
+            to each client. With more than 100 happy clients, I take pride in
+            delivering personalized and high-quality services that leave my
+            clients feeling pampered and confident. Whether it's a classic look
+            or a unique design, I strive to make every manicure and pedicure a
+            masterpiece.
+          </p>
+        </div>
+      </div>
+    </section>
   );
 }
