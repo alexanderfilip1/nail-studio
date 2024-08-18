8015b5e108555fec2c4117f1cdc36d8deb8b4db0
alexanderfilip1 <showgammer@gmail.com>
2024-08-18 18:15:02 +0300

    Create UI for Services Section

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Header from "../components/Header";
 import HeroSection from "../components/HeroSection";
 import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
 export default function HomePage() {
   const [mobileHeader, setMobileHeader] = useState(false);
   return (
@@ -10,6 +11,7 @@ export default function HomePage() {
       <main className={mobileHeader ? "main blur" : "main"}>
         <HeroSection />
         <AboutSection />
+        <ServicesSection />
       </main>
     </>
   );
