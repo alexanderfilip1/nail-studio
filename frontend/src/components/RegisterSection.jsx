f631f1224596f5b19389501b5834e896c6a5b6bb
alexanderfilip1 <showgammer@gmail.com>
2024-08-20 15:38:59 +0300

    Improve Register UI

--- frontend/src/components/RegisterSection.jsx
+++ frontend/src/components/RegisterSection.jsx
@@ -1,11 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
+import "../assets/css/Register.css";
+import AuthBtn from "./AuthBtn";
 
 export default function RegisterSection() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegister = async () => {
+    const registerData = {
+      username: username,
+      email: email,
+      password: password,
+    };
+    try {
+      const req = await fetch("http://localhost:3000/api/register", {
+        method: "POST",
+        body: JSON.stringify(registerData),
+      });
+      const body = await req.json();
+      console.log(body);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <>
-      <section className="register__section container bgBrown">
-        <form>
-          <input type="text" placeholder="Username" />
+      <section className="register__section">
+        <form
+          className="form"
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleRegister();
+          }}
+        >
+          <label htmlFor="username">
+            <input
+              type="text"
+              placeholder="Username"
+              id="username"
+              className="input"
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+            />
+          </label>
+          <label htmlFor="email">
+            <input
+              type="text"
+              placeholder="Email"
+              id="email"
+              className="input"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+          </label>
+          <label htmlFor="password">
+            <input
+              type="text"
+              placeholder="Password"
+              id="password"
+              className="input"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+          </label>
+          <AuthBtn action={"Register"} />
         </form>
       </section>
     </>
