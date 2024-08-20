ff84475a5637c0f323ba7fa6faef38981031f11a
alexanderfilip1 <showgammer@gmail.com>
2024-08-20 16:13:00 +0300

    Add preloader and error handling

---
+++ frontend/src/components/Loader.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../assets/css/Loader.css";
+export default function Loader() {
+  return (
+    <>
+      <div className="wrapper">
+        <div id="preloader">
+          <div id="loader"></div>
+        </div>
+      </div>
+    </>
+  );
+}
