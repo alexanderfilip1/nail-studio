2aca5a15ce30b56eed142e82cb5484c82940c85c
alexanderfilip1 <showgammer@gmail.com>
2024-08-20 12:27:00 +0300

    Implement Register Section

---
+++ frontend/src/components/RegisterSection.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function RegisterSection() {
+  return (
+    <>
+      <section className="register__section container bgBrown">
+        <form>
+          <input type="text" placeholder="Username" />
+        </form>
+      </section>
+    </>
+  );
+}
