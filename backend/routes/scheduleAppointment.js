90b5a1fcbcf4f128ff82b5b61bf0b044ef6eabf1
alexanderfilip1 <showgammer@gmail.com>
2024-08-22 19:23:15 +0300

    Calculate total price for all services

--- backend/routes/scheduleAppointment.js
+++ backend/routes/scheduleAppointment.js
@@ -5,26 +5,44 @@ const db = require("../config/db");
 router.post("/", async function (req, res, next) {
   const { name, phone, date, service } = req.body;
   console.log(service);
+  let lastTime;
+  let totalTime = 0;
   try {
     const [result] = await db.query(
       "INSERT INTO appointments (name, phone, start_datetime, end_datetime) VALUES (?, ?, ?, ?)",
       [name, phone, date, date]
     );
     const appointmentId = result.insertId;
-    service.forEach(async (item) => {
-      console.log(item);
-      const [id] = await db.query("SELECT id FROM prices WHERE name = ?", [
-        item,
-      ]);
-      const serviceId = id[0].id;
-      await db.query(
-        "INSERT INTO appointment_services (appointment_id, service_id) VALUES (?, ?)",
-        [appointmentId, serviceId]
-      );
-    });
+
+    await Promise.all(
+      service.map(async (item) => {
+        console.log(item);
+
+        const [id] = await db.query("SELECT id FROM prices WHERE name = ?", [
+          item,
+        ]);
+
+        const [time] = await db.query(
+          "SELECT required_time FROM prices WHERE name = ?",
+          [item]
+        );
+
+        lastTime = time[0].required_time;
+        totalTime += +lastTime;
+        const serviceId = id[0].id;
+        await db.query(
+          "INSERT INTO appointment_services (appointment_id, service_id) VALUES (?, ?)",
+          [appointmentId, serviceId]
+        );
+      })
+    );
+
+    console.log(`Total Time for All Services: ${totalTime}`);
+
     res.status(201).json("Success");
   } catch (err) {
     console.log(err);
+    res.status(500).json("Error occurred");
   }
 });
 
