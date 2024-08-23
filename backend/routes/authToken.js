a5d15b3048910e7694e487981d746318b61fad07
alexanderfilip1 <showgammer@gmail.com>
2024-08-23 16:42:57 +0300

    Add jwt to auth token route

--- backend/routes/authToken.js
+++ backend/routes/authToken.js
@@ -1,18 +1,17 @@
 var express = require("express");
 var router = express.Router();
+const jwt = require("jsonwebtoken");
 
 router.get("/", (req, res) => {
   if (req.cookies.token) {
     try {
       const decoded = jwt.verify(req.cookies.token, process.env.JWT_SECRET);
-      res
-        .status(200)
-        .json({ status: "success", message: "authenticated", user: decoded });
+      res.status(200).json({ message: "authenticated", user: decoded });
     } catch (err) {
-      res.status(401).json({ status: "error", message: "unauthenticated" });
+      res.status(401).json({ message: "unauthenticated" });
     }
   } else {
-    res.status(401).json({ status: "unauthenticated" });
+    res.status(401).json({ message: "unauthenticated" });
   }
 });
 
