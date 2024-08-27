3e2b30ebd3058d3004ce37d23e513a62d1a2f398
alexanderfilip1 <showgammer@gmail.com>
2024-08-27 19:52:14 +0300

    Add functionality to check if user is admin

--- frontend/src/pages/AdminPanel.jsx
+++ frontend/src/pages/AdminPanel.jsx
@@ -2,15 +2,24 @@ import { useEffect, useState } from "react";
 import Header from "../components/Header";
 import AdminPanelSection from "../components/AdminPanelSection";
 import useAuthToken from "../hooks/useAuthToken";
+import { useNavigate } from "react-router-dom";
 
 export default function AdminPanel() {
   const [mobileHeader, setMobileHeader] = useState(false);
   const [isAdmin, setIsAdmin] = useState(false);
+  const [countdown, setCountdown] = useState(3);
   const userStatus = useAuthToken();
+  const navigate = useNavigate();
 
-  const checkIfAdmin = async () => {
+  const checkIfAdmin = async (id) => {
     try {
-      const req = await fetch("http://localhost:3000/api/adminCheck");
+      const req = await fetch("http://localhost:3000/api/checkAdmin", {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify({ userID: id }),
+      });
+      const body = await req.json();
+      setIsAdmin(body.isAdmin);
     } catch (err) {
       console.log(err);
     }
@@ -22,21 +31,41 @@ export default function AdminPanel() {
     }
 
     if (userStatus.status === true && userStatus.user) {
-      console.log(userStatus);
-      setIsAdmin(true);
+      checkIfAdmin(userStatus.user.id);
     } else if (userStatus.status === false) {
       console.log("User is not authenticated");
     } else if (userStatus.status === "error") {
       console.error("Error verifying authentication");
     }
   }, [userStatus]);
+
+  useEffect(() => {
+    if (!isAdmin) {
+      const intervalId = setInterval(() => {
+        setCountdown((prev) => prev - 1);
+      }, 1000);
+
+      if (countdown === 0) {
+        navigate("/");
+      }
+
+      return () => clearInterval(intervalId);
+    }
+  }, [isAdmin, countdown, navigate]);
+
   return (
     <>
       <Header mobileHeader={mobileHeader} setMobileHeader={setMobileHeader} />
       <main
         className={mobileHeader ? "main bgGradient blur " : "main bgGradient"}
       >
-        {isAdmin && <AdminPanelSection />}
+        {isAdmin ? (
+          <AdminPanelSection />
+        ) : (
+          <h1 className="notAuthorized">
+            Not Authorized! Redirecting in {countdown}...
+          </h1>
+        )}
       </main>
     </>
   );
