dbceffcf9905bfd676c2acb8fb23a00c4ee1133a
alexanderfilip1 <showgammer@gmail.com>
2024-08-28 18:24:47 +0300

    Implement user show and user delete for admin

--- frontend/src/components/AdminPanelSection.jsx
+++ frontend/src/components/AdminPanelSection.jsx
@@ -1,12 +1,43 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../assets/css/AdminPanelSection.css";
 
 export default function AdminPanelSection() {
   const [activeSection, setActiveSection] = useState("Users");
+  const [userList, setUserList] = useState([]);
 
   const handleSectionChange = (section) => {
     setActiveSection(section);
   };
+
+  const getUserList = async () => {
+    try {
+      const req = await fetch("http://localhost:3000/api/admin/users", {
+        method: "GET",
+        credentials: "include",
+      });
+      const body = await req.json();
+      setUserList(body);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const removeUser = async (id) => {
+    try {
+      const req = await fetch(`http://localhost:3000/api/admin/users/${id}`, {
+        method: "DELETE",
+        credentials: "include",
+      });
+      const body = await req.json();
+      console.log(body);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getUserList();
+  }, [userList]);
 
   return (
     <section className="admin__section container">
@@ -47,7 +78,45 @@ export default function AdminPanelSection() {
         </ul>
       </aside>
       <article className="admin__content">
-        {activeSection === "Users" && <h1>All Users</h1>}
+        {activeSection === "Users" && (
+          <>
+            <h1>
+              All Users{userList.length > 0 ? `(${userList.length})` : ""}
+            </h1>
+            <ul className="user__list">
+              {userList.map((user) => {
+                const { id, username, email, balance, appointment, phone } =
+                  user;
+                return (
+                  <li key={id} className="user__list--item">
+                    <h3>Username: {username}</h3>
+                    <p>Email :{email}</p>
+                    <p>Balance: {balance}</p>
+                    <p>Appointments: {appointment}</p>
+                    <p>Phone Number: {phone}</p>
+                    <button
+                      className="close__btn"
+                      aria-label="Remove user"
+                      onClick={() => {
+                        removeUser(id);
+                      }}
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        style={{ fill: "red" }}
+                      >
+                        <path d="M18.3 5.71c-.39-.39-1.02-.39-1.41 0L12 10.59 7.11 5.7c-.39-.39-1.02-.39-1.41 0-.39.39-.39 1.02 0 1.41L10.59 12l-4.88 4.88c-.39.39-.39 1.02 0 1.41.39.39 1.02.39 1.41 0L12 13.41l4.88 4.88c.39.39 1.02.39 1.41 0 .39-.39.39-1.02 0-1.41L13.41 12l4.88-4.88c.38-.39.38-1.03 0-1.41z" />
+                      </svg>
+                    </button>
+                  </li>
+                );
+              })}
+            </ul>
+          </>
+        )}
         {activeSection === "Appointments" && <h1>All Appointments</h1>}
         {activeSection === "Services" && <h1>All Services</h1>}
         {activeSection === "Settings" && <h1>Settings</h1>}
