bffb516b5d06862ffdd7c9e6f80f002337461da3
alexanderfilip1 <showgammer@gmail.com>
2024-08-30 21:03:56 +0300

    Implement leave a review functionality

--- frontend/src/components/ReviewSection.jsx
+++ frontend/src/components/ReviewSection.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Slider from "react-slick";
 import "../assets/css/ReviewSection.css";
 import { FaStar } from "react-icons/fa";
@@ -6,6 +6,11 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 export default function ReviewSection() {
+  const [leaveReview, setLeaveReview] = useState(false);
+  const [rating, setRating] = useState(0);
+  const [reviewerName, setReviewerName] = useState("");
+  const [reviewText, setReviewText] = useState("");
+
   const reviews = [
     {
       id: 1,
@@ -45,6 +50,24 @@ export default function ReviewSection() {
     pauseOnHover: true,
   };
 
+  const createReview = async () => {
+    try {
+      const req = await fetch("http://localhost:3000/api/createReview", {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify({
+          rating: rating,
+          name: reviewerName,
+          text: reviewText,
+        }),
+      });
+      const body = await req.json();
+      console.log(body);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <section className="review-section container bgBeige">
       <h1 className="section-title">Reviews</h1>
@@ -68,6 +91,66 @@ export default function ReviewSection() {
           );
         })}
       </Slider>
+      {!leaveReview && (
+        <button
+          className="btn"
+          style={{ width: "100%", marginTop: "2rem" }}
+          onClick={() => setLeaveReview(true)}
+        >
+          Leave a review
+        </button>
+      )}
+      {leaveReview && (
+        <>
+          <form action="" className="review__form">
+            <label htmlFor="name">
+              <h3>Name</h3>
+              <input
+                type="text"
+                id="name"
+                className="input"
+                onChange={(e) => setReviewerName(e.target.value)}
+              />
+            </label>
+            <label htmlFor="review">
+              <h3>Review</h3>
+              <textarea
+                name="reviewText"
+                id="review"
+                className="textarea__review input"
+                maxLength={100}
+                cols="30"
+                placeholder="Very satisfied"
+                rows={5}
+                onChange={(e) => setReviewText(e.target.value)}
+              ></textarea>
+            </label>
+            <label htmlFor="stars">
+              <h3>Star Rate</h3>
+              <div className="star-rating">
+                {Array(5)
+                  .fill()
+                  .map((_, i) => (
+                    <FaStar
+                      key={i}
+                      className={`star ${rating > i ? "filled" : ""}`}
+                      onClick={() => setRating(i + 1)}
+                    />
+                  ))}
+              </div>
+            </label>
+            <div className="review__actionBtns">
+              <button className="btn review__btn">Submit</button>
+              <button
+                className="btn review__btn"
+                onClick={() => setLeaveReview(false)}
+              >
+                Cancel
+              </button>
+            </div>
+          </form>
+        </>
+      )}
     </section>
   );
 }
